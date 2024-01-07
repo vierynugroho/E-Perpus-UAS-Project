a8a6d59e45af0eb0f28a19bbfd93cff27adb6423
Viery Nugroho <viery15102002@gmail.com>
2024-01-07 09:50:15 +0700

    datatable

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -10,7 +10,7 @@
         <meta name="csrf-token"
               content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
         <link rel="dns-prefetch"
@@ -21,10 +21,48 @@
         <!-- Scripts -->
         @viteReactRefresh
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+        <!-- Bootstrap CSS v5.2.1 -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+              rel="stylesheet"
+              integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+              crossorigin="anonymous" />
+
+        {{-- Datatables --}}
+        <link rel="stylesheet"
+              href="{{ asset('DataTables/DataTables-1.13.8/css/dataTables.bootstrap5.css') }}">
     </head>
 
     <body>
         @yield('body')
+
+
+        {{-- ! SCRIPT AREA ! --}}
+        <!-- Bootstrap JavaScript Libraries -->
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+                integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r"
+                crossorigin="anonymous"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"
+                integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+"
+                crossorigin="anonymous"></script>
+
+        {{-- JQuery --}}
+        <script src="{{ asset('DataTables/jQuery-3.7.0/jquery-3.7.0.min.js') }}"></script>
+
+        {{-- Datatables --}}
+        <script src="{{ asset('DataTables/DataTables-1.13.8/js/jquery.dataTables.min.js') }}"></script>
+        <script src="{{asset('DataTables/DataTables-1.13.8/js/dataTables.bootstrap5.min.js')}}"></script>
+
+        {{-- DataTable Config --}}
+        <script>
+            $(document).ready(function() {
+                        $('#dataTable').DataTable();
+                    });
+        </script>
+
+        {{-- AddOn Script --}}
+        @stack('addOnScript')
     </body>
 
 </html>
