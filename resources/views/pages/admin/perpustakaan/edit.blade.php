46cca5c2317941464dc0df39f7c3e0ce3d82afd2
Viery Nugroho <viery15102002@gmail.com>
2024-01-08 13:34:56 +0700

    some fix

--- resources/views/pages/admin/perpustakaan/edit.blade.php
+++ resources/views/pages/admin/perpustakaan/edit.blade.php
@@ -4,7 +4,7 @@
 <!-- Begin Page Content -->
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Input Buku</h1>
+    <h1 class="h3 mb-0 text-gray-800">Edit Buku</h1>
 </div>
 
 
