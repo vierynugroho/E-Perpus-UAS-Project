b3b88843b49d2e7aafd9c05c10f23b3cca464ebd
Viery Nugroho <viery15102002@gmail.com>
2024-01-08 07:57:03 +0700

    dashboard view and settings

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -111,51 +111,43 @@
                 <h6 class="m-0 font-weight-bold text-primary">Leaderboard</h6>
             </div>
             <div class="card-body">
-                <h4 class="small font-weight-bold">Server Migration <span class="float-right">20%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar bg-danger"
-                         role="progressbar"
-                         style="width: 20%"
-                         aria-valuenow="20"
-                         aria-valuemin="0"
-                         aria-valuemax="100"></div>
-                </div>
-                <h4 class="small font-weight-bold">Sales Tracking <span class="float-right">40%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar bg-warning"
-                         role="progressbar"
-                         style="width: 40%"
-                         aria-valuenow="40"
-                         aria-valuemin="0"
-                         aria-valuemax="100"></div>
-                </div>
-                <h4 class="small font-weight-bold">Customer Database <span class="float-right">60%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar"
-                         role="progressbar"
-                         style="width: 60%"
-                         aria-valuenow="60"
-                         aria-valuemin="0"
-                         aria-valuemax="100"></div>
-                </div>
-                <h4 class="small font-weight-bold">Payout Details <span class="float-right">80%</span></h4>
-                <div class="progress mb-4">
-                    <div class="progress-bar bg-info"
-                         role="progressbar"
-                         style="width: 80%"
-                         aria-valuenow="80"
-                         aria-valuemin="0"
-                         aria-valuemax="100"></div>
-                </div>
-                <h4 class="small font-weight-bold">Account Setup <span class="float-right">Complete!</span></h4>
-                <div class="progress">
-                    <div class="progress-bar bg-success"
-                         role="progressbar"
-                         style="width: 100%"
-                         aria-valuenow="100"
-                         aria-valuemin="0"
-                         aria-valuemax="100"></div>
-                </div>
+                <ul class="list-group">
+                    <li class="list-item list-unstyled">
+                        <div class="row">
+                            <div class="col-2">
+                                <p>1 👑</p>
+                            </div>
+                            <div class="col-10">
+                                <p>King</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-item list-unstyled">
+                        <div class="row">
+                            <div class="col-2">
+                                <p>2</p>
+                            </div>
+                            <div class="col-10">
+                                <p>Sepuh</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-item list-unstyled">
+                        <div class="row">
+                            <div class="col-2">
+                                <p>3</p>
+                            </div>
+                            <div class="col-10">
+                                <p>Pemula</p>
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-item text-center list-unstyled">
+                        <a href="/dashboard/leaderboard">
+                            <small>Lihat Selengkapnya</small>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
---
+++ resources/views/pages/setting.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.base_dashboard')
+
+@section('content_dashboard')
+<h1>Setting</h1>
+@endsection
