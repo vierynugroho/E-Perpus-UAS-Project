7ff877ab7e41220d263f90a233125c963a88e359
Viery Nugroho <viery15102002@gmail.com>
2024-01-11 14:16:32 +0700

    view home fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,107 +3,104 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="row">
-            <div class="col-10 mx-auto">
-                <div class="row p-5 rounded mx-auto shadow hero">
-                    <div class="col-6 col-sm-12 mx-auto my-5 w-100">
-                        <form class="d-flex"
-                              role="search"
-                              @if($cari_buku===null)
-                              action='{{ route('home') }}'
-                              @else
-                              action='{{ route('cari_buku', $cari_buku) }}'
-                              @endif
-                              method="GET">
-                            @csrf
-                            <input class="form-control me-2"
-                                   type="search"
-                                   placeholder="Cari Buku"
-                                   aria-label="Search"
-                                   name="cari_buku"
-                                   value="{{ $cari_buku }}">
-                            <button class="btn btn-outline-light d-flex justify-content-center align-items-center"
-                                    type="submit"><i class="fas fa-search"></i> Cari</button>
-                        </form>
+        <div class="col-10 mx-auto">
+            <div class="row p-5 rounded mx-auto shadow hero">
+                <div class="col-6 col-sm-12 mx-auto my-5 w-100">
+                    <form class="d-flex"
+                          role="search"
+                          @if($cari_buku===null)
+                          action='{{ route('home') }}'
+                          @else
+                          action='{{ route('cari_buku', $cari_buku) }}'
+                          @endif
+                          method="GET">
+                        @csrf
+                        <input class="form-control me-2"
+                               type="search"
+                               placeholder="Cari Buku"
+                               aria-label="Search"
+                               name="cari_buku"
+                               value="{{ $cari_buku }}">
+                        <button class="btn btn-outline-light d-flex justify-content-center align-items-center"
+                                type="submit"><i class="fas fa-search"></i> Cari</button>
+                    </form>
+                </div>
+            </div>
+            <div class="row shadow kategori-content mx-auto mt-4">
+                <div class="row">
+                    <div class="col-12">
+                        <h4 class="fw-bold">KATEGORI</h4>
+                    </div>
+                    <div class="divider">
+                        <br>
                     </div>
                 </div>
-                <div class="container shadow kategori-content row mt-4">
-                    <div class="row">
-                        <div class="col-12">
-                            <h4 class="fw-bold">KATEGORI</h4>
-                        </div>
-                        <div class="divider">
-                            <br>
-                        </div>
+                <div class="row d-flex flex-nowrap overflow-auto">
+                    @forelse ($categories as $category)
+                    <div class="col-6 col-md-3">
+                        <a href="/homebycategory/{{ $category->id }}"
+                           class="text-decoration-none">
+                            <div class="card shadow rounded mb-3 category text-bg-dark">
+                                <div class="card-body mx-auto">
+                                    <h6>{{ $category->name }}</h6>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                    <div class="row d-flex flex-nowrap overflow-auto">
-                        @forelse ($categories as $category)
-                        <div class="col-6 col-md-3">
-                            <a href="/homebycategory/{{ $category->id }}"
-                               class="text-decoration-none">
-                                <div class="card shadow rounded mb-3 category text-bg-dark">
-                                    <div class="card-body mx-auto">
-                                        <h6>{{ $category->name }}</h6>
+                    @empty
+                    <p class="text-center text-muted">- TIDAK ADA BUKU -</p>
+                    @endforelse
+                </div>
+            </div>
+            <div class="row shadow book-content mx-auto mt-4">
+                <div class="row">
+                    <div class="col-12 mt-3">
+                        <h4 class="fw-bold">PERPUSTAKAAN</h4>
+                    </div>
+                    <div class="divider">
+                        <br>
+                    </div>
+                </div>
+                <div class="row d-flex mx-auto">
+                    @foreach ($datas as $data)
+                    <div class="col-12 col-md-6 col-lg-4">
+                        <div class="card book shadow rounded p-3 m-3 d-flex">
+                            <div class="row w-100">
+                                <div class="col-6">
+                                    <div class="book-cover">
+                                        <img src="{{ Storage::url($data->cover) }}"
+                                             alt="Cover Buku"
+                                             class="img-fluid">
                                     </div>
                                 </div>
-                            </a>
-                        </div>
-                        @empty
-                        <p class="text-center text-muted">- TIDAK ADA BUKU -</p>
-                        @endforelse
-                    </div>
-                </div>
-                <div class="container shadow book-content row my-5 p-2">
-                    <div class="row">
-                        <div class="col-12">
-                            <h4 class="fw-bold">PERPUSTAKAAN</h4>
-                        </div>
-                        <div class="divider">
-                            <br>
-                        </div>
-                    </div>
-                    <div class="row d-flex mx-auto">
-                        @foreach ($datas as $data)
-                        <div class="col-12 col-md-6 col-lg-4">
-                            <div class="card book shadow rounded p-3 m-3 d-flex">
-                                <div class="row w-100">
-                                    <div class="col-6">
-                                        <div class="book-cover">
-                                            <img src="{{ Storage::url($data->cover) }}"
-                                                 alt="Cover Buku"
-                                                 class="img-fluid">
-                                        </div>
-                                    </div>
 
-                                    <div class="col-6">
-                                        <div class="book-detail">
+                                <div class="col-6">
+                                    <div class="book-detail">
 
-                                            <h6 class="fw-bold">{{ $data->judul }}</h6>
-                                            <hr>
-                                            <p><span class="fw-bold">Penulis: </span> {{ $data->penulis }}</p>
-                                        </div>
+                                        <h6 class="fw-bold">{{ $data->judul }}</h6>
+                                        <hr>
+                                        <p><span class="fw-bold">Penulis: </span> {{ $data->penulis }}</p>
                                     </div>
-                                    <div class="col-12 m-2">
-                                        @if ($data->quantity > 0)
-                                        <h6 class="fw-bold">Stock: <span>{{ $data->quantity }}</span></h6>
-                                        @else
-                                        <h6 class="text-danger fw-bold text-center">TIDAK TERSEDIA</h6>
-                                        @endif
-                                    </div>
-                                    <div class="book-action">
-                                        <a href="/detail/{{ $data->id }}"
-                                           class="list-unstyled text-decoration-none text-dark">
-                                            <button class="btn btn-primary d-block w-100 fw-bold">Detail</button>
-                                        </a>
-                                    </div>
+                                </div>
+                                <div class="col-12 m-2">
+                                    @if ($data->quantity > 0)
+                                    <h6 class="fw-bold">Stock: <span>{{ $data->quantity }}</span></h6>
+                                    @else
+                                    <h6 class="text-danger fw-bold text-center">TIDAK TERSEDIA</h6>
+                                    @endif
+                                </div>
+                                <div class="book-action">
+                                    <a href="/detail/{{ $data->id }}"
+                                       class="list-unstyled text-decoration-none text-dark">
+                                        <button class="btn btn-primary d-block w-100 fw-bold">Detail</button>
+                                    </a>
                                 </div>
                             </div>
                         </div>
-                        @endforeach
                     </div>
-                    {{ $datas->links('pagination::bootstrap-5') }}
+                    @endforeach
                 </div>
-
+                {{ $datas->links('pagination::bootstrap-5') }}
             </div>
         </div>
     </div>
--- resources/views/homeByCategory.blade.php
+++ resources/views/homeByCategory.blade.php
@@ -27,61 +27,57 @@
                         </form>
                     </div>
                 </div>
-                <div class="container shadow book-content row my-5">
-                    <div class="row">
-                        <div class="col-12">
-                            <h4>PERPUSTAKAAN : {{ $category->name }}</h4>
-                        </div>
-                        <div class="divider">
-                            <br>
-                        </div>
+                <div class="row shadow kategori-content mx-auto mt-4">
+                    <div class="col-12 mt-3">
+                        <h4 class="fw-bold">PERPUSTAKAAN : {{ $category->name }}</h4>
                     </div>
-                    <div class="row d-flex mx-auto">
-                        @forelse ($datas as $data)
-                        <div class="col-12 col-md-6 col-lg-4">
+                    <div class="divider">
+                        <br>
+                    </div>
+                    @forelse ($datas as $data)
+                    <div class="col-12 col-md-6 col-lg-4">
 
-                            <div class="card book shadow rounded p-3 m-3 d-flex">
-                                <div class="row w-100">
-                                    <div class="col-6">
-                                        <div class="book-cover">
-                                            <img src="{{ Storage::url($data->cover) }}"
-                                                 alt="Cover Buku {{ $data->judul }}"
-                                                 class="img-fluid">
-                                        </div>
-                                    </div>
-
-                                    <div class="col-6">
-                                        <div class="book-detail">
-
-                                            <h6 class="fw-bold">{{ $data->judul }}</h6>
-                                            <hr>
-                                            <p><span class="fw-bold">Penulis: </span> {{ $data->penulis }}</p>
-                                        </div>
-                                    </div>
-                                    <div class="col-12 m-2">
-                                        @if ($data->quantity > 0)
-                                        <h6>Stock: <span>{{ $data->quantity }}</span></h6>
-                                        @else
-                                        <h6 class="text-danger fw-bold text-center">TIDAK TERSEDIA</h6>
-                                        @endif
-                                    </div>
-                                    <div class="book-action">
-                                        <a href="/detail/{{ $data->id }}"
-                                           class="list-unstyled text-decoration-none text-dark">
-                                            <button class="btn btn-primary d-block w-100 fw-bold">Detail</button>
-                                        </a>
+                        <div class="card book shadow rounded p-3 m-3 d-flex">
+                            <div class="row w-100">
+                                <div class="col-6">
+                                    <div class="book-cover">
+                                        <img src="{{ Storage::url($data->cover) }}"
+                                             alt="Cover Buku {{ $data->judul }}"
+                                             class="img-fluid">
                                     </div>
                                 </div>
 
+                                <div class="col-6">
+                                    <div class="book-detail">
+
+                                        <h6 class="fw-bold">{{ $data->judul }}</h6>
+                                        <hr>
+                                        <p><span class="fw-bold">Penulis: </span> {{ $data->penulis }}</p>
+                                    </div>
+                                </div>
+                                <div class="col-12 m-2">
+                                    @if ($data->quantity > 0)
+                                    <h6>Stock: <span>{{ $data->quantity }}</span></h6>
+                                    @else
+                                    <h6 class="text-danger fw-bold text-center">TIDAK TERSEDIA</h6>
+                                    @endif
+                                </div>
+                                <div class="book-action">
+                                    <a href="/detail/{{ $data->id }}"
+                                       class="list-unstyled text-decoration-none text-dark">
+                                        <button class="btn btn-primary d-block w-100 fw-bold">Detail</button>
+                                    </a>
+                                </div>
                             </div>
+
                         </div>
-                        @empty
-                        <h1 class="text-center text-muted p-5">- TIDAK ADA BUKU -</h1>
-                        @endforelse
                     </div>
-                    {{ $datas->links('pagination::bootstrap-5') }}
+                    @empty
+                    <h1 class="text-center text-muted p-5">- TIDAK ADA BUKU -</h1>
+                    @endforelse
                 </div>
             </div>
+            {{ $datas->links('pagination::bootstrap-5') }}
         </div>
     </div>
 </div>
